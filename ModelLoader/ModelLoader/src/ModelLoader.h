6220c52c8dc4afb0d1bbcfecc6fbbb3e7b2d78dc
Kacper Mazur <kacper.mazur888@gmail.com>
2019-11-29 02:13:40 +0000

    Model Refactor With error checking

---
+++ ModelLoader/ModelLoader/src/ModelLoader.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <unordered_map>
+
+#include "glm/vec3.hpp"
+#include "glm/ext.hpp"
+#include "glm/gtx/string_cast.hpp"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
